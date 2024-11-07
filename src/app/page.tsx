f2366dc9fc21843ba27979d57261d0ebab164012
Radha Sharma <radhasharma2786@gmail.com>
2024-11-07 21:51:01 +0530

    designing hero section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,10 @@
+import HeroSection from "@/components/HeroSection";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.02] ">
-    <h1 className="text-2xl text-center ">Radha's Website</h1>
+    <main className="min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.02] py-10 md:py-0 overflow-hidden">
+    <HeroSection/>
     </main>
   );
 }
